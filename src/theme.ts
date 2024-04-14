5befde80029cf164905af3947514d52b4b46f1dd
hijunglit <hijunglit@gmail.com>
2024-04-14 17:53:07 +0900

    push todo list

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+    textColor: "#ff7979",
+    backgroundColor: "#2c3e50",
+    btnColor: "#eb4d4b",
+}
